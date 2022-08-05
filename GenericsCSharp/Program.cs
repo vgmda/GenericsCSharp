47356d5d6c79819d46c9a188b288ebfb81227141
vgmda <vasilegancin@gmail.com>
2022-08-05 23:30:51 +0100

    Implement PopulateLists()

--- GenericsCSharp/Program.cs
+++ GenericsCSharp/Program.cs
@@ -25,8 +25,28 @@ class Program
         string peopleFile = @"./people.csv";
         string logFile = @"./logs.csv";
 
-        PopulateLists(people, logFile);
+        // PopulateLists(people, logFile);
+
+        OriginalTextFileProcessor.SavePeople(people, peopleFile);
+
+        var newPeople = OriginalTextFileProcessor.LoadPeople(peopleFile);
+
+        foreach (var p in newPeople)
+        {
+            Console.WriteLine($"{p.FirstName} {p.LastName} (IsAlive = {p.IsAlive})");
+        }
     }
 
+
+    private static void PopulateLists(List<Person> people, List<LogEntry> logs)
+    {
+        people.Add(new Person { FirstName = "Tim", LastName = "Corey" });
+        people.Add(new Person { FirstName = "Sue", LastName = "Storm", IsAlive = false });
+        people.Add(new Person { FirstName = "Greg", LastName = "Olsen" });
+
+        logs.Add(new LogEntry { Message = "I blew up", ErrorCode = 9999 });
+        logs.Add(new LogEntry { Message = "I'm too awesome", ErrorCode = 1337 });
+        logs.Add(new LogEntry { Message = "I was tired", ErrorCode = 2222 });
+    }
 
 }
