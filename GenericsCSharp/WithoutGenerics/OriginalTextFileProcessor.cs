615f854670eb838413e0fe4da6be2310ad08846a
vgmda <vasilegancin@gmail.com>
2022-08-06 12:01:56 +0100

    New file

---
+++ GenericsCSharp/WithoutGenerics/OriginalTextFileProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenericsCSharp.Models;
+
+namespace GenericsCSharp.WithoutGenerics;
+
+public class OriginalTextFileProcessor
+{
+    public static List<Person> LoadPeople(string filePath)
+    {
+
+    }
+
+    public static void SavePeople(List<Person> people, string filePath)
+    {
+
+    }
+
+
+}
+
