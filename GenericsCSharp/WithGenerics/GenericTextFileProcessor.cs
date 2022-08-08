a0d02313e7a66fc175ebeb5585c642079ce2f517
vgmda <vasilegancin@gmail.com>
2022-08-08 23:24:39 +0100

    Implement SaveToTextFile()

--- GenericsCSharp/WithGenerics/GenericTextFileProcessor.cs
+++ GenericsCSharp/WithGenerics/GenericTextFileProcessor.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace GenericsCSharp.WithGenerics;
 
 public static class GenericTextFileProcessor
@@ -44,5 +49,43 @@ public static class GenericTextFileProcessor
         return output;
 
     }
+
+
+    public static void SaveToTextFile<T>(List<T> data, string filePath) where T : class
+    {
+        List<string> lines = new List<string>();
+        StringBuilder line = new StringBuilder();
+
+        if (data == null || data.Count == 0)
+        {
+            throw new ArgumentNullException("data", "You must populate the data parameter with at least one value.");
+        }
+
+        var cols = data[0].GetType().GetProperties();
+
+        foreach (var col in cols)
+        {
+            line.Append(col.Name);
+            line.Append(",");
+        }
+
+        lines.Add(line.ToString().Substring(0, line.Length - 1));
+
+        foreach (var row in data)
+        {
+            line = new StringBuilder();
+
+            foreach (var col in cols)
+            {
+                line.Append(col.GetValue(row));
+                line.Append(',');
+            }
+
+            lines.Add(line.ToString().Substring(0, line.Length - 1));
+        }
+
+        System.IO.File.WriteAllLines(filePath, lines);
+    }
+
 }
 
